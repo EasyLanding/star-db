8b03cda2bf380369419698c08d3c3933486b5be0
Georgiy Antonevich <goshka.ruu@mail.ru>
2022-07-25 16:11:36 +0300

    add
    Signed-off-by: Georgiy Antonevich <goshka.ruu@mail.ru>

--- src/Components/random-planet/random-planet.js
+++ src/Components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Spinner from '../spinner';
 import SwapiService from '../../services/swapi-service';
+import ErrorIndicator from '../error-indicator';
 
 import './random-planet.css';
 
@@ -25,26 +26,41 @@ export default class RandomPlanet extends Component
     {
         this.setState({
             planet,
-            loading: false
+            loading: false,
+            error: false
         });
     };
 
+    onError = (err) =>
+    {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
     updatePlanet ()
     {
-        const id = 12;
+        const id = 15;
         this.swapiService
             .getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
 
     render ()
     {
-        const { planet, loading } = this.state;
+        const { planet, loading, error } = this.state;
+
+        const hasData = !(loading || error)
+
+        const errorMessage = error ? < ErrorIndicator /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <PlanetView planet={ planet } /> : null;
+        const content = hasData ? <PlanetView planet={ planet } /> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
+                { errorMessage }
                 { spinner }
                 { content }
             </div>
